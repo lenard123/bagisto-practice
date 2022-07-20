f892be5bb387a7bf2837857e95e2c72a0ec30230
Lenard  Mangay-ayam <31153286+lenard123@users.noreply.github.com>
2022-07-20 16:11:12 +0800

    Add Pull Command

--- app/Console/Commands/PullRepo.php
+++ app/Console/Commands/PullRepo.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
 
 class PullRepo extends Command
 {
@@ -11,22 +12,93 @@ class PullRepo extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'git:pull';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Pull Github Repository';
+
+    /**
+     * Is the code already updated or not
+     * 
+     * @var boolean
+     */
+    private $alreadyUpToDate;
+
+
+    /**
+     * Log from git pull
+     * 
+     * @var array
+     */
+    private $pullLog = [];
+
+    /**
+     * Log from composer install
+     * 
+     * @var boolean
+     */
+    private $composerLog = [];
 
     /**
      * Execute the console command.
      *
-     * @return int
+     * @return mixed
      */
     public function handle()
     {
-        return 0;
+
+        if(!$this->runPull()) {
+
+            $this->error("An error occurred while executing 'git pull'. \nLogs:");
+
+            foreach($this->pullLog as $logLine) {
+                $this->info($logLine);
+            }
+
+            return;
+        }
+
+
+        if($this->alreadyUpToDate) {
+            $this->info("The application is already up-to-date");
+            return;
+        }
+
+
+        $this->info("Succesfully updated the application.");
+
+
+    }
+
+
+
+
+    /**
+     * Run git pull process
+     * 
+     * @return boolean
+     */
+
+    private function runPull()
+    {
+
+        $process = new Process('git pull');
+        $this->info("Running 'git pull'");
+
+        $process->run(function($type, $buffer) {
+            $this->pullLog[] = $buffer;
+
+            if($buffer == "Already up to date.\n") {
+                $this->alreadyUpToDate = TRUE;
+            }
+            
+        });
+
+        return $process->isSuccessful();
+
     }
 }
